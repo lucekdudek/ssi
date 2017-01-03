63582169a607a4d13af6f49953f0b6ce0bcd9c6e
Bartosz Wilk <bartek94@gmail.com>
2017-01-03 23:28:01 +0100

    Time fixed

--- PortalAukcyjny/src/main/resources/static/js/offer.js
+++ PortalAukcyjny/src/main/resources/static/js/offer.js
@@ -1,39 +1,17 @@
-$('.product-details form').on('submit', function(event){
-	event.preventDefault();
-	
-	var currentPrice = $('.product-details__bid span').text();
-	
-	var form = $(this);
-	var input = form.find('input[name="price"]');
-	
-	input.css('color', '#000000');
-	
-	if(input.val()<=currentPrice) return input.css('color', '#ff0000');
-	
-	$.ajax({
-		method: "POST",
-		url: form.attr('action'),
-		data: { price: input.val() }
-	})
-	.done(function( price ) {
-		form[0].reset();
-		$('.product-details__bid span').text(price);
-	});
-})
-
 $(function(){
 	var dateSpan = /([0-9]{4})-([0-9]{2})-([0-9]{2})\s([0-9]{2}):([0-9]{2})/g.exec($('#date strong').text());
 	var endDate = new Date(dateSpan[1], dateSpan[2] - 1, dateSpan[3], dateSpan[4], dateSpan[5]);
 	var currentDate = new Date();
-	var timeDiff = Math.abs(currentDate.getTime() - endDate.getTime());
+	var timeDiff = endDate.getTime() - currentDate.getTime();
 	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 	var diffHours = Math.ceil(timeDiff / (1000 * 3600)); 
 	var diffMinutes = Math.ceil(timeDiff / (1000 * 60)); 
 	var diffSeconds = Math.ceil(timeDiff / 1000); 
+	
 
 	function countingMinutes(){
 		currentDate = new Date();
-		timeDiff = Math.abs(currentDate.getTime() - endDate.getTime());
+		timeDiff = endDate.getTime() - currentDate.getTime();
 		diffMinutes = Math.ceil(timeDiff / (1000 * 60)); 
 		$('#date strong').text('za '+diffMinutes+' minuty');
 		if(diffSeconds > 1){
@@ -45,15 +23,16 @@ $(function(){
 	
 	function countingSeconds(){
 		currentDate = new Date();
-		timeDiff = Math.abs(currentDate.getTime() - endDate.getTime());
+		timeDiff = endDate.getTime() - currentDate.getTime();
 		diffSeconds = Math.ceil(timeDiff / 1000);
 		$('#date strong').text('za '+diffSeconds+' sekundy');
 		if(diffSeconds > 1){
 			setTimeout(countingSeconds, 500);
+		}else{
+			$('.product-details form').css('display', 'none');
+			$('#date').text('Aukcja się skończyła! :(');
 		}
 	}
-	
-	console.log(diffDays);
 	
 	if(diffDays <= 1){
 		if(diffHours > 1) {
@@ -65,9 +44,33 @@ $(function(){
 				if(diffSeconds > 1){
 					countingSeconds();
 				}else{
+					$('.product-details form').css('display', 'none');
 					$('#date').text('Aukcja się skończyła! :(');
 				}
 			}
 		}
 	}
+	
+	$('.product-details form').on('submit', function(event){
+		event.preventDefault();
+		
+		var currentPrice = $('.product-details__bid span').text();
+		
+		var form = $(this);
+		var input = form.find('input[name="price"]');
+		
+		input.css('color', '#000000');
+		
+		if(input.val()<=currentPrice) return input.css('color', '#ff0000');
+		
+		$.ajax({
+			method: "POST",
+			url: form.attr('action'),
+			data: { price: input.val() }
+		})
+		.done(function( price ) {
+			form[0].reset();
+			$('.product-details__bid span').text(price);
+		});
+	})
 })
